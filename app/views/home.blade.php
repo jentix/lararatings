e943d0821f9f6a0cd184d5c3a92d8a2342333dfe
Boris <jentix@yandex.ru>
2013-06-20 14:58:28 +0400

    default css add

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -6,8 +6,6 @@
 @stop
 
 @section('content')
-	<h1>Привет</h1>
-	
 	@foreach ($ratings as $rating)
 		<p>Имя: {{ $rating->name }} дата: {{ $rating->date }}</p>
 	@endforeach
--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -1,21 +1,36 @@
-<html>
-<head>
+<!DOCTYPE html>
+<html lang="ru">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="rating system by caddim">
 	<title>
-		@section('title') 
-			Lararating 
-		@show
+	@section('title') 
+	Lararating 
+	@show
 	</title>
 	
 	<link rel="shortcut icon" href="favicon.png">
+	<link href="css/default.css" rel="stylesheet" type="text/css">
 
 	<!-- bootstrap css -->
 	<link href="css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<link href="css/bootstrap/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script> 
+        <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+    <![endif]-->
 </head>
 <body>
-	<div class="container"> 
-		@yield('content')
+	<div class="container">
+		<header>
+			<h1>Laravel rating system</h1>
+		</header>
+		<div id="content"> 
+			@yield('content')
+		</div>
 	</div>
+	
 	<!-- jQuery -->
 	<script src="js/jquery-1.10.1.min.js"></script>
 	<!-- bootstrap js -->
