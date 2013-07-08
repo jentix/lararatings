2a68bacaadb7e2d639f7bdca5206226a25fbd4c2
Boris <jentix@yandex.ru>
2013-07-08 15:27:37 +0400

    add update

--- app/views/add.blade.php
+++ app/views/add.blade.php
@@ -7,6 +7,7 @@
 
 @section('content')
 	@if ($login)
+		<div id="left-call-add">
 		<form>
 			<label class="control-label" for="inputName"><strong>Название</strong></label>
 			<input name="name" id="inputName" type="text" class="span4" placeholder="Name" maxlength="60">
@@ -19,6 +20,23 @@
 			
 			<br><button id="addbutton" type="submit" name="add" class="btn btn-large">Добавить</button>
 		</form>
+
+		@if (isset($messages))
+			<div class="alert alert-error add-messages">
+				<button type="button" class="close" data-dismiss="alert">&times;</button>
+				@foreach ($messages as $message)
+			    	<p>{{$message}}</p>
+			    @endforeach
+			</div>
+		@endif
+
+		@if (isset($success))
+			<div class="alert alert-success add-messages">
+				<button type="button" class="close" data-dismiss="alert">&times;</button>
+				{{$success}}
+			</div>
+		@endif
+		</div>
 	@else 
 		<div id="log_btns_group">
 			<h4>Что-бы добавить сайт, авторизируйтесь</h4>
@@ -28,23 +46,6 @@
 			</span>
 		</div> 
 	@endif
-
-	@if (isset($messages))
-		<div class="alert alert-error login-errors">
-			<button type="button" class="close" data-dismiss="alert">&times;</button>
-			@foreach ($messages as $message)
-		    	<p>{{$message}}</p>
-		    @endforeach
-		</div>
-	@endif
-
-	@if (isset($success))
-		<div class="alert alert-success login-errors">
-			<button type="button" class="close" data-dismiss="alert">&times;</button>
-			{{$success}}
-		</div>
-	@endif
-
 @stop
 
 @section('js')
