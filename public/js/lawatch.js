24a89ae86199925f128f01a33ed6e0091208de68
Boris <jentix@yandex.ru>
2013-07-11 16:49:30 +0400

    new touch script

--- public/js/lawatch.js
+++ public/js/lawatch.js
@@ -7,7 +7,7 @@ watcher.touch = function() {
 	else {
 	   xhttp=new ActiveXObject("Microsoft.XMLHTTP");
 	}
-	xhttp.open('POST','touch',true);
+	xhttp.open('POST','touch.php',true);
 	xhttp.setRequestHeader('Content-type','application/x-www-form-urlencoded');
 	var data = 'id='+(this.id ? this.id : '0');
 	xhttp.send(data);
--- public/touch.php
+++ public/touch.php
@@ -3,36 +3,14 @@
 		echo "Ошибка подключения к Mysql";
 		exit;
 	}
-	mysql_select_db('lararating');
-	$input = array(
-		'ip' => ip2long($_SERVER['REMOTE_ADDR']),
-		'referer' => $_SERVER['HTTP_REFERER'],
-		'uniq_str' => md5($_SERVER['REMOTE_ADDR'].$_SERVER['HTTP_USER_AGENT']),
-		'time' => time()
-	);
-	if (isset($_POST['id'])) $input['site_id'] = intval($_POST['id']);
-	else $input['site_id'] = 0;
-
-	echo var_export($input);
-	insertArray('clients', $input);
-	// $q = mysql_query("INSERT INTO CLIENTS VALUES (".$input['site_id'].", ".$input['ip'].", ".$input['referer'].", ".$input['uniq_str'].", ".$input['time'].")");
-	// echo $q;
-
-	function insertArray($table,$data) {
-		foreach ($data AS $v=>$k) {
-				$k = mysql_real_escape_string($k);
-				if (!isset($what))	{
-					@$what	= "`$v`";
-					@$value	.= ($k=='NOW()') ? "$k" : "'$k'";				
-				}
-				else {
-					@$what	.= ",`$v`";
-					@$value	.= ($k=='NOW()') ? ", $k" : ",'$k'";
-				}
-			}
-			$sql="INSERT INTO `$table`
-							  ($what)
-				  VALUE	  	  ($value)
-				 ";
-	}
+	mysql_select_db('lararating') or die(mysql_error());
+	
+	$ip = ip2long($_SERVER['REMOTE_ADDR']);
+	$referer = $_SERVER['HTTP_REFERER'];
+	$uniq_str = md5($_SERVER['REMOTE_ADDR'].$_SERVER['HTTP_USER_AGENT']);
+	$site_id = intval($_POST['id']);
+	$time = time();
+	
+	$query = "INSERT INTO clients VALUES('$site_id','$ip','$referer','$uniq_str','$time')";
+	mysql_query($query) or die(mysql_error()); 
 ?>
